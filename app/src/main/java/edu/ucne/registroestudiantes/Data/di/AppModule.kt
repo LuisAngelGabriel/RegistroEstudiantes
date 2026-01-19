1523bca1850f3ad9b5e03ef31fb33aa5c1ddebf5
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-01-19 09:03:15 -0600

    Creando el module

---
+++ app/src/main/java/edu/ucne/registroestudiantes/Data/di/AppModule.kt
@@ -0,0 +1,50 @@
+package edu.ucne.registroestudiantes.Data.di
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registroestudiantes.Data.DataBase.EstudianteDB
+import edu.ucne.registroestudiantes.Data.Local.EstudianteDao
+import edu.ucne.registroestudiantes.Data.Repository.EstudianteRepositoryImpl
+import edu.ucne.registroestudiantes.Domain.Repository.EstudianteRepository
+import jakarta.inject.Singleton
+
+
+@InstallIn(
+    SingletonComponent::class)
+@Module
+
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideEstudianteDB(@ApplicationContext appContext: Context) : EstudianteDB{
+
+        return Room.databaseBuilder(
+            appContext,
+            EstudianteDB::class.java,
+            "EstudianteDB"
+        ).fallbackToDestructiveMigration(false)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideEstudianteDao(estudianteDB: EstudianteDB) : EstudianteDao{
+        return estudianteDB.estudianteDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideEstudianteRepositoryImpl(estudianteDao: EstudianteDao) : EstudianteRepositoryImpl{
+        return EstudianteRepositoryImpl(estudianteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEstudianteRepository(impl: EstudianteRepositoryImpl): EstudianteRepository {
+        return impl
+    }
+}
