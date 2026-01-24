f2c80823323faf53a90c5cf3d313bd74814ceb99
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-01-24 01:15:18 -0600

    Agregando asignaturas al app module

--- app/src/main/java/edu/ucne/registroestudiantes/Data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/Data/di/AppModule.kt
@@ -7,9 +7,12 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.registroestudiantes.Data.DataBase.EstudianteDB
+import edu.ucne.registroestudiantes.Data.Local.AsignaturaDao
 import edu.ucne.registroestudiantes.Data.Local.EstudianteDao
+import edu.ucne.registroestudiantes.Data.Repository.AsignaturaRepositoryImpl
 import edu.ucne.registroestudiantes.Data.Repository.EstudianteRepositoryImpl
-import edu.ucne.registroestudiantes.Domain.Repository.EstudianteRepository
+import edu.ucne.registroestudiantes.Domain.Asignaturas.Repository.AsignaturaRepository
+import edu.ucne.registroestudiantes.Domain.Estudiantes.Repository.EstudianteRepository
 import jakarta.inject.Singleton
 
 
@@ -45,6 +48,24 @@ object AppModule {
     @Provides
     @Singleton
     fun provideEstudianteRepository(impl: EstudianteRepositoryImpl): EstudianteRepository {
+        return impl
+    }
+
+    @Provides
+    @Singleton
+    fun provideAsignaturaDao(estudianteDB: EstudianteDB): AsignaturaDao {
+        return estudianteDB.asignaturaDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAsignaturaRepositoryImpl(asignaturaDao: AsignaturaDao): AsignaturaRepositoryImpl {
+        return AsignaturaRepositoryImpl(asignaturaDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAsignaturaRepository(impl: AsignaturaRepositoryImpl): AsignaturaRepository {
         return impl
     }
 }
