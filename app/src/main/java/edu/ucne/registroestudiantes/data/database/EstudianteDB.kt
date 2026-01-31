564b04a66c7bfe6936cb56d3b78d7b859e2c03f2
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-01-31 08:54:12 -0600

    Registrando nueva entidad "TipoPenalidad"

--- app/src/main/java/edu/ucne/registroestudiantes/data/database/EstudianteDB.kt
+++ app/src/main/java/edu/ucne/registroestudiantes/data/database/EstudianteDB.kt
@@ -4,20 +4,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registroestudiantes.data.local.dao.AsignaturaDao
 import edu.ucne.registroestudiantes.data.local.dao.EstudianteDao
+import edu.ucne.registroestudiantes.data.local.dao.TipoPenalidadDao
 import edu.ucne.registroestudiantes.data.local.entities.EstudianteEntity
 import edu.ucne.registroestudiantes.data.local.entities.AsignaturaEntity
+import edu.ucne.registroestudiantes.data.local.entities.TipoPenalidadEntity
+
 
 
 @Database(
     entities = [
         EstudianteEntity::class,
-        AsignaturaEntity::class
+        AsignaturaEntity::class,
+        TipoPenalidadEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false)
 
 abstract class EstudianteDB: RoomDatabase(){
 
     abstract fun estudianteDao(): EstudianteDao
     abstract fun asignaturaDao(): AsignaturaDao
+
+    abstract fun TipoPenalidadDao(): TipoPenalidadDao
 }
